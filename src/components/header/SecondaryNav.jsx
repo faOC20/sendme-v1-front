df7e12b3c2d0b9844d7e1a97c444246061a527a4
faOC20 <fabianortizfc@gmail.com>
2024-05-01 12:50:59 -0400

    retoque del header
    se retoco el nav secundario, ahora esta centrado

--- src/components/header/SecondaryNav.jsx
+++ src/components/header/SecondaryNav.jsx
@@ -5,8 +5,8 @@ import { ShoppingCartButton } from "./ShoppingCartButton"
 export const SecondaryNav = ()=>{
     return(
        <div class="flex-grow content-center">
-         <nav class="flex-grow content-end h-3/6">
-            <ul class="flex h-full">
+         <nav class="flex-grow content-end h-full">
+            <ul class="flex h-full items-center">
                 
                 <FiatChanger/>
                 
